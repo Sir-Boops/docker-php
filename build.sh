114ff2df997730b1347c2aca536f733cdd2f3862
Sir_Boops <admin@boops.me>
2018-07-05 13:29:17 -0600

    Remove drone

---
+++ build.sh
@@ -0,0 +1,21 @@
+TAGS="7.2.7 latest"
+DOCKER_REPO="sirboops/php"
+
+BUILD_TAGS=""
+
+# Gen build tags
+for t in $TAGS;
+do
+    BUILD_TAGS="`echo $BUILD_TAGS` `echo -t $DOCKER_REPO:$t`"
+done
+
+
+# Build the image
+docker build $BUILD_TAGS . --squash
+
+
+# Upload the images
+for t in $TAGS;
+do
+    docker push `echo $DOCKER_REPO:$t`
+done
